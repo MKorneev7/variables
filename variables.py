af3e9cba7d6bf6428a56c6469431cf1076000a85
MKorneev7 <mik61349304@yandex.ru>
2024-08-26 01:12:17 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,9 @@
+first_name='Python,'
+number_of_tasks='12,'
+number_of_hours_spent='1.5,'
+print(1.5/12)
+time_for_one_task=(1.5/12)
+print (first_name,'всего задач:',number_of_tasks,'затрачено часов',number_of_hours_spent, 'cреднее время выполнения' ,time_for_one_task, 'часа' )
+
+
+
